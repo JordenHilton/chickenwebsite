474674588cb2f4caeacb2e629b6b31173c91ceba
Jorden Hilton <jorden.hilton@gmail.com>
2021-03-13 13:38:42 -0600

    update dev dependency

---
+++ src/components/Feature/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+const Feature = () => {
+    return (
+        <FeatureContainer><h1>Try our Special</h1>
+        <p>Truffle alfredo sauce with 24 carrot gold dust</p>
+        <FeatureButton>Order Now</FeatureButton>
+        </FeatureContainer>
+    )
+}
+
+export default Feature
